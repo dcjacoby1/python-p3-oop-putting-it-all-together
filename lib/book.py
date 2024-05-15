16917627463a2e9c377a2fd5e691462ec7f6b84b
Dan <jacobydan01@gmail.com>
2024-05-15 17:57:53 -0400

    objectRequiredLab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+    
+    #property only used for constraints
+    #in this case to force int. otherwise, not needed
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, page_count):
+        if type(page_count) == int:
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+    
+    #when called, prints statemenet => needs self arg
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+
+    
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    #brand and size required to add object, condition optional
+    def __init__(self, brand, size, condition = 'Old'):
+        self.brand = brand
+        self.size = size
+        self.condition = condition
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    #once cobbled, changes to condition to new
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = 'New'
+
+
